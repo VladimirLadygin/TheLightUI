a366b1f0ac04689e6c806ae6b412854a92d042b1
Vladimir Ladygin <ladygin@lenvoku.ru>
2020-11-01 11:04:35 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Владимир Ладыгин on 01.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
